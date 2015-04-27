26699dc95f9032a4face2207688ce5dfa5d70dca
Ramon Blanquer <hello@ramonblanquer.com>
2015-04-28 00:40:42 +0100

    Parser works with my scene. I will try to break it now.

--- FinalSubmission/include/Parser.h
+++ FinalSubmission/include/Parser.h
@@ -7,14 +7,20 @@
 #include <fstream>
 #include <streambuf>
 #include <vector>
+#include <ngl/Vec3.h>
+#include <ngl/Colour.h>
 
 #include "Light.h"
 #include "Shape.h"
+#include "Plane.h"
+#include "Sphere.h"
 
 
 class Parser
 {
+
 public:
+
   Parser(    std::string &_image_name,
              std::string &_text_file,
              int &_width,
@@ -47,15 +53,21 @@ public:
       tokenizer tok{text,quote};
       tokenizer::iterator it = tok.begin();
       std::string tmp;
+
+      std::cout << "Parsing filename...\n";
       ++it;
       _image_name = *it;
+      std::cout << "OK! Filename: " << _image_name << std::endl;
       ++it; ++it;
+      std::cout << "Parsing file dimensions...\n";
       tmp = *it;
       _width = atoi(tmp.c_str()); // WIDTH
       ++it; ++it;
       tmp = *it;
       _height = atoi(tmp.c_str()); // HEIGHT
+      std::cout << "OK! " << _width << "x" << _height << " image specified.\n";
       ++it; ++it;
+      std::cout << "Parsing camera...\n";
       tmp = *it;
       _camPosX = atof(tmp.c_str()); // camPosX
       ++it; ++it;
@@ -73,6 +85,7 @@ public:
       ++it; ++it;
       tmp = *it;
       _lookAtZ = atof(tmp.c_str()); // lookAtZ
+      std::cout << "OK! Camera parsed with pos["<<_camPosX <<","<<_camPosY<<","<<_camPosZ<<"] and lookup["<<_lookAtX<<","<<_lookAtY<<","<<_lookAtZ<<"]\n";
       ++it; ++it;
       tmp = *it;
       _max_depth = atoi(tmp.c_str()); // max_depth
@@ -80,15 +93,58 @@ public:
       _anti_aliasing = atoi(tmp.c_str()); // anti_aliasing
       ++it; ++it;
 
-      parseLights(text);
+      std::cout << "Parsing lights...\n";
+      parseLights(text, _scene_lights);
+
+      std::cout << "Parsing objects...\n";
+      parseObjects(text, _scene_objects);
 
 
   }
   ~Parser() {}
 
-  void parseObjects(){}
+  void parseObjects(std::string& _text, std::vector<geo::Shape*> &_scene_objects)
+  {
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> dollar{"$"};
+    tokenizer tok{_text,dollar};
+    tokenizer::iterator it = tok.begin();
+    it++;
 
-  void parseLights(std::string& _text)
+    std::string tmp;
+    tmp = *it;
+
+
+
+
+
+    while(it != tok.end())
+    {
+      tmp = *it;
+      if (tmp.at(0) == '}') break;
+      if (tmp.at(0) == 'P')
+      {
+        // parse plane
+        parsePlane(tmp,_scene_objects);
+      }
+      else if (tmp.at(0) == 'S')
+      {
+        // parse sphere
+        parseSphere(tmp,_scene_objects);
+      }
+      ++it;
+    }
+
+
+
+
+
+
+
+
+  }
+
+  void parseLights(std::string& _text, std::vector<Light*> &_scene_lights)
   {
     typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
     boost::char_separator<char> plus{"+"};
@@ -98,25 +154,222 @@ public:
 
     std::string tmp;
 
-
     while(it != tok.end())
     {
       tmp = *it;
       if (tmp.at(0) == '}') break;
-      parseSingleLight(tmp);
+      parseSingleLight(tmp, _scene_lights);
       ++it;
+    }
+  }
+
+  void parseSingleLight(std::string &_text_light, std::vector<Light*> &_scene_lights)
+  {
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> space{" "};
+    tokenizer tok{_text_light,space};
+    tokenizer::iterator it = tok.begin();
+    std::string tmp;
+    std::string light_name;
+
+    ngl::Vec3 position;
+    ngl::Colour diffuse_col;
+    ngl::Colour specular_col;
+    float diff_int;
+    float spec_int;
+    float falloff;
+    light_name = *it;
+    it++;it++;
+    tmp = *it;
+    position.m_x = atof(tmp.c_str());
+    it++;
+    tmp = *it;
+    position.m_y = atof(tmp.c_str());
+    it++;
+    tmp = *it;
+    position.m_z = atof(tmp.c_str());
+    it++;it++;it++;
+    tmp = *it;
+
+    diffuse_col.m_r = atof(tmp.c_str()); it++;
+    tmp = *it; diffuse_col.m_g = atof(tmp.c_str()); it++;
+    tmp = *it; diffuse_col.m_b = atof(tmp.c_str()); it++;it++;it++;
+    tmp = *it; specular_col.m_r = atof(tmp.c_str()); it++;
+    tmp = *it; specular_col.m_g = atof(tmp.c_str()); it++;
+    tmp = *it; specular_col.m_b = atof(tmp.c_str()); it++;it++;
+
+    tmp = *it;
+    diff_int = atof(tmp.c_str());
+    it++;
+
+
+    tmp = *it;
+    spec_int = atof(tmp.c_str());
+    it++;
+
+
+    tmp = *it;
+    falloff = atof(tmp.c_str());
+
+
+
+    PointLight* light = new PointLight(position, diffuse_col, specular_col, diff_int, spec_int, falloff);
+    _scene_lights.push_back(light);
+    std::cout << "OK! Light with name " << light_name << " has been parsed.\n";
+  }
+
+  void parsePlane(std::string& _text, std::vector<geo::Shape*> &_scene_objects)
+  {
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> space{" "};
+    tokenizer tok{_text,space};
+    tokenizer::iterator it = tok.begin();
+    ++it;
+
+    std::string tmp;
+
+    float nX, nY, nZ;
+    float distance;
+
+    std::string plane_name = *it; it++;
+
+    tmp = *it; distance = atof(tmp.c_str());
+    it++;it++;
+    tmp = *it; nX = atof(tmp.c_str()); it++;
+    tmp = *it; nY = atof(tmp.c_str()); it++;
+    tmp = *it; nZ = atof(tmp.c_str()); it++;it++;
+    tmp = *it;
+    if (tmp == "@checker")
+    {
+      float col1_r, col1_g, col1_b, col2_r, col2_g, col2_b;
+      it++;it++;
+      tmp = *it; col1_r = atof(tmp.c_str());it++;
+      tmp = *it; col1_g = atof(tmp.c_str());it++;
+      tmp = *it; col1_b = atof(tmp.c_str());it++; it++; it++;
+      tmp = *it; col2_r = atof(tmp.c_str());it++;
+      tmp = *it; col2_g = atof(tmp.c_str());it++;
+      tmp = *it; col2_b = atof(tmp.c_str());it++; it++;
+      tmp = *it;
+      if (tmp == "@specularHardness")
+      {
+        it++;
+        float specularHardness;
+        tmp = *it; specularHardness = atof(tmp.c_str());
+        geo::Shape* plane = new geo::Plane(distance,ngl::Vec3(nX,nY,nZ),ngl::Colour(col1_r,col1_g,col1_b,1), ngl::Colour(col2_r,col2_g,col2_b,1));
+        plane->getMaterial()->m_spec_hardness = specularHardness;
+        _scene_objects.push_back(plane);
+        std::cout << "OK! Plane " << plane_name << " has been parsed successfully.\n";
+      }
+      else
+      {
+        geo::Shape* plane = new geo::Plane(distance,ngl::Vec3(nX,nY,nZ),ngl::Colour(col1_r,col1_g,col1_b,1), ngl::Colour(col2_r,col2_g,col2_b,1));
+        _scene_objects.push_back(plane);
+        std::cout << "OK! Plane " << plane_name << " has been parsed successfully.\n";
+      }
+
+
+      //std::cout << "DEBUGGING --> " << *it << std::endl;
 
 
     }
-
+    else
+    {
+      float col_r, col_g, col_b;
+      it++;
+      tmp = *it; col_r = atof(tmp.c_str()); it++;
+      tmp = *it; col_g = atof(tmp.c_str()); it++;
+      tmp = *it; col_b = atof(tmp.c_str()); it++; it++;
+      tmp = *it;
+      if(tmp == "@specularHardness")
+      {
+        it++;
+        float specularHardness;
+        tmp = *it; specularHardness = atof(tmp.c_str());
+        geo::Shape* plane = new geo::Plane(distance,ngl::Vec3(nX,nY,nZ),ngl::Colour(col_r,col_g,col_b,1));
+        plane->getMaterial()->m_spec_hardness = specularHardness;
+        _scene_objects.push_back(plane);
+      }
+      else
+      {
+        geo::Shape* plane = new geo::Plane(distance,ngl::Vec3(nX,nY,nZ),ngl::Colour(col_r,col_g,col_b,1));
+        _scene_objects.push_back(plane);
+      }
+    }
   }
 
-  void parseSingleLight(std::string &_text_light){
+  void parseSphere(std::string& _text, std::vector<geo::Shape*> &_scene_objects)
+  {
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> space{" "};
+    tokenizer tok{_text,space};
+    tokenizer::iterator it = tok.begin();
+    ++it;
 
-    std::cout << _text_light << std::endl;
+    std::string tmp;
+
+    float posX, posY, posZ;
+    float colX, colY, colZ;
+    float radius;
+
+
+    std::string sphere_name = *it;
+    it++;it++;
+
+
+    tmp = *it; posX = atof(tmp.c_str()); it++;
+    tmp = *it; posY = atof(tmp.c_str()); it++;
+    tmp = *it; posZ = atof(tmp.c_str()); it++;it++;
+
+    tmp = *it; radius = atof(tmp.c_str()); it++;it++;
+    tmp = *it; colX = atof(tmp.c_str()); it++;
+    tmp = *it; colY = atof(tmp.c_str()); it++;
+    tmp = *it; colZ = atof(tmp.c_str()); it++;it++;
+
+    tmp = *it;
+    if(tmp == "@specularHardness") {
+      it++;
+
+      tmp = *it;
+      float specularHardness = atof(tmp.c_str());
+      geo::Shape* sphere = new geo::Sphere(ngl::Vec3(posX,posY,posZ),radius, ngl::Colour(colX,colY,colZ,1));
+      sphere->getMaterial()->m_spec_hardness = specularHardness;
+      _scene_objects.push_back(sphere);
+      std::cout << "OK! Sphere " << sphere_name << " has been parsed successfully.\n";
+
+    }
+    else if(tmp == "@reflective") {
+      it++;
+      tmp = *it;
+      float reflection_ratio = atof(tmp.c_str()) / 100.0f;
+
+      geo::Shape* sphere = new geo::Sphere(ngl::Vec3(posX,posY,posZ),radius, ngl::Colour(colX,colY,colZ,1));
+      sphere->hasReflection(reflection_ratio, 1.0f-reflection_ratio);
+      _scene_objects.push_back(sphere);
+      std::cout << "OK! Sphere " << sphere_name << " has been parsed successfully.\n";
+    }
+    else if(tmp == "@refractive") {
+      it++;
+      tmp = *it;
+      float ior = atof(tmp.c_str());
+      it++;
+      tmp = *it;
+      float refraction_ratio = atof(tmp.c_str()) / 100.0f;
+
+      geo::Shape* sphere = new geo::Sphere(ngl::Vec3(posX,posY,posZ),radius, ngl::Colour(colX,colY,colZ,1));
+      sphere->hasRefraction(ior, refraction_ratio, 1.0f-refraction_ratio);
+      _scene_objects.push_back(sphere);
+      std::cout << "OK! Sphere " << sphere_name << " has been parsed successfully.\n";
+    }
+    else {
+      geo::Shape* sphere = new geo::Sphere(ngl::Vec3(posX,posY,posZ),radius, ngl::Colour(colX,colY,colZ,1));
+      _scene_objects.push_back(sphere);
+    }
+
+
+    //geo::Shape* sphere1 = new geo::Sphere(ngl::Vec3(0,0,2),float(0.8f), ngl::Colour(1,0,1,1));
+
 
   }
-  void parseSingleObject(){}
 
 };
 
--- FinalSubmission/src/main.cpp
+++ FinalSubmission/src/main.cpp
@@ -63,74 +63,23 @@ int main(int argc, char *argv[])
   // initialise scene
   Scene myScene;
 
-  // define some colours
-  ngl::Colour white(1,1,1,1);
-  ngl::Colour black(0,0,0,1);
-  ngl::Colour red(1,0,0,1);
-  ngl::Colour green(0,1,0,1);
-  ngl::Colour blue(0,0,1,1);
-  ngl::Colour light_blue(0.347,0.89,0.78,1);
 
-  // set up some lights
-  PointLight* light1 = new PointLight(ngl::Vec3(1,5,2),light_blue,light_blue,1,1,1);
-  myScene.addLight(light1);
+  for(unsigned int i = 0; i < scene_objects.size(); i++)
+  {
+    myScene.addObject(scene_objects.at(i));
+  }
 
-  PointLight* light2 = new PointLight(ngl::Vec3(-1,0.4,0),white,white,0.5,0.5,1);
-  myScene.addLight(light2);
+  for(unsigned int i = 0; i < scene_lights.size(); i++)
+  {
+    myScene.addLight(scene_lights.at(i));
+  }
 
-  PointLight* light3 = new PointLight(ngl::Vec3(-1.5,1,10),red,red,1,1,1);
-  myScene.addLight(light3);
 
-  // Lay down some geometry
-  geo::Shape* plane1 = new geo::Plane(-1.0f,ngl::Vec3(0,1,0),ngl::Colour(0,0,0,1), ngl::Colour(1,1,1,1));
-  plane1->getMaterial()->m_spec_hardness = 4.0f;
-  myScene.addObject(plane1);
-
-  /* CORNELL BOX
-  // FRONT PLANE
-  geo::Shape* plane2 = new geo::Plane(-8.0f,ngl::Vec3(0,0,-1),ngl::Colour(1,1,1,1)*0.5);
-  myScene.addObject(plane2);
-  // LEFT PLANE
-  geo::Shape* plane3 = new geo::Plane(-2.0f,ngl::Vec3(1,0,0),ngl::Colour(0.403,0.770,0.760,1));
-  myScene.addObject(plane3);
-  // RIGHT PLANE
-  geo::Shape* plane4 = new geo::Plane(-2.0f,ngl::Vec3(-1,0,0),ngl::Colour(1,0.56,0,1));
-  myScene.addObject(plane4);
-  // TOP PLANE
-  geo::Shape* plane5 = new geo::Plane(-2.0f,ngl::Vec3(0,-1,0),ngl::Colour(1,1,1,1));
-  myScene.addObject(plane5);
-  // BACK PLANE
-  geo::Shape* plane6 = new geo::Plane(-2.0f,ngl::Vec3(0,0,1),ngl::Colour(0.5f,0,0,1));
-  myScene.addObject(plane6);*/
-
-  geo::Shape* sphere1 = new geo::Sphere(ngl::Vec3(0,0,2),float(0.8f), ngl::Colour(1,0,1,1));
-  sphere1->getMaterial()->m_spec_hardness = 5.0f;
-  myScene.addObject(sphere1);
-
-  geo::Shape* sphere2 = new geo::Sphere(ngl::Vec3(-1,1.2,2),float(0.6f), ngl::Colour(0.1,0.4,0.95,1));
-  sphere2->hasReflection(0.7f, 0.3f);
-  myScene.addObject(sphere2);
-
-  geo::Shape* sphere3 = new geo::Sphere(ngl::Vec3(0.9,0.15,2.4),float(0.3f), ngl::Colour(0,1,0,1));
-  sphere3->getMaterial()->m_spec_hardness = 100.0f;
-  myScene.addObject(sphere3);
-
-  geo::Shape* sphere4 = new geo::Sphere(ngl::Vec3(-0.9,0.15,0.8),float(0.3f), ngl::Colour(1,0,0,1));
-  sphere4->hasRefraction(1.2f, 1.0f, 0.0f);
-  myScene.addObject(sphere4);
-
-  geo::Shape* sphere5 = new geo::Sphere(ngl::Vec3(0.5,0.15,0.2),float(0.2f), ngl::Colour(0,0.8,1,1));
-  sphere5->getMaterial()->m_spec_hardness = 40.0f;
-  myScene.addObject(sphere5);
 
   // initialise film
   Film myFilm(width,height);
 
-  ngl::Vec3 O (0,0,0);
-  ngl::Vec3 X (1,0,0);
   ngl::Vec3 Y (0,1,0);
-  ngl::Vec3 Z (0,0,1);
-
   ngl::Vec3 campos(camPosX,camPosY,camPosZ);
   ngl::Vec3 lookat(lookAtX,lookAtY,lookAtZ);
 
@@ -146,10 +95,10 @@ int main(int argc, char *argv[])
 
 
   // initialise renderer and bind film and camera to it
-  //Renderer renderer(myScene, myFilm, myCamera, max_depth, anti_aliasing);
+  Renderer renderer(myScene, myFilm, myCamera, max_depth, anti_aliasing);
 
   // start the rendering process
-  //renderer.render();
+  renderer.render();
 
   // display the image
 
